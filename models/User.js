b79f40d0a1ebdd7e1cb513ede4cabae13226c083
WiZe (Yaroslav V. Petrov) <yaroslav.v.petrov@yandex.ru>
2018-11-30 01:23:43 +0100

    Fixed an error inside the user model

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     passwordHash: {type: String, required: true},
     dateCreated: {type: Date, default: Date.now, required:true},
-    avatar:{ype: String},
+    avatar:{type: String},
     boards: {type: Array, default:[], required: true},
     assignedTasks: {type: Array, default:[], required:true}
 });
