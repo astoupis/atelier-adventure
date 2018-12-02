a32f6b61f9d46f06fb925c412ed0773eae62a77a
WiZe (Yaroslav V. Petrov) <yaroslav.v.petrov@yandex.ru>
2018-12-02 03:22:49 +0100

    created simple login-logout system from existing routes

--- routes/login/router.js
+++ routes/login/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-
+const config = require('../../config');
 const auth = require("../../util/auth");
 
 const mongoose = require('mongoose');
@@ -18,11 +18,24 @@ router.get('/', function(req, res) {
 //Login action (get sesssion/create authentification token)
 router.post('/', function(req, res) {
     auth.login(req.body.username, req.body.password)
-    .then(function(result) {
-            res.json({ success: true, token: result, message: "Enjoy your token!" });
+    .then(function(tokenString) {
+        res.set("Set-Cookie", "token=" + tokenString + "; Max-Age=" + config.auth.expirationInSeconds);
+
+        if(req.accepts("html")) res.redirect("/");
+        else res.json({ 
+            success: true, 
+            token: tokenString, 
+            message: "Enjoy your token!" , 
+        });
     })
     .catch(function(error) {
-        res.json({ success: false, message: error.message });
+        console.log("CATCH")
+        res.set("Set-Cookie", "token=invalid.cookie.value");
+        if(req.accepts("html")) res.redirect("/");
+        else res.json({
+            success: false, 
+            message: error,
+        });
     });
 });
 
--- routes/root/router.js
+++ routes/root/router.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
-
-const mongoose = require('mongoose');
+const auth = require("../../util/auth");
 
 //GET METHOD
 //If session is active get the user page
 //If session is not active, get the login/regsitration page
 router.get('/', function(req, res) {
-
+    // THIS ROUTE IS NOW USED FOR VERIFICATION TESTING
+    auth.authenticate(req)
+    .then(function(payload) {
+        res.json(payload);
+    })
+    .catch(function(error) {
+        res.json(error);
+    });
 });
 
 module.exports = router;
