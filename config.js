c8bd09cb1c89936c4643e758f32160c22ffc1cae
WiZe (Yaroslav V. Petrov) <yaroslav.v.petrov@yandex.ru>
2018-12-02 03:21:47 +0100

    added authenticate function to the auth module

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ module.exports = {
     database: 'mongodb://localhost/atelier-adventure',
 
     auth: {
+        type: 'Bearer',
+        cookie: 'token',
         secret: 'followthelight',
         expirationInSeconds: 60 * 60 * 12, // 12h00m00s
     }
--- util/auth/index.js
+++ util/auth/index.js
@@ -3,5 +3,6 @@ module.exports = {
     "AuthErrors" : require("./AuthErrors"),
     "TokenPayload" : require("./TokenPayload"),
     "login" : require("./login"),
+    "authenticate" : require("./authenticate"),
 };
 
