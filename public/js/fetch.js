41ddbd93cc327b13c13929c8af01416080abbaa2
WiZe (Yaroslav V. Petrov) <yaroslav.v.petrov@yandex.ru>
2018-12-16 18:56:02 +0100

    fixed the query of the list name change feat Alex

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -326,10 +326,10 @@ function boardGetLists(boardId, wipe=false) {
                     let hiddenDiv = document.createElement('div');
                     hiddenDiv.className = "hidden-div";
                     listSpace.parentElement.insertBefore(hiddenDiv, listSpace);
-                    child.firstElementChild.addEventListener('blur', (element) => {
+                    child.querySelector("h1.state-head").addEventListener('blur', (element) => {
                         element = element.srcElement;
                         let listName = element.innerHTML;
-                        let listId = element.parentNode.id;
+                        let listId = element.parentNode.parentNode.id;
                         let boardId = document.querySelector(".droptarget-column").id;
                         doJSONRequest('PUT', "/list", {'Content-Type': 'application/json'}, 
                         {boardId: boardId,
